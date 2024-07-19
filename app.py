48cb8c421d27e2aaf96f8e6a3d12dc52647912ba
Muhammad Guntur Arfianto <72558038+TRUEnder@users.noreply.github.com>
2024-07-19 15:30:20 +0700

    Fix dependencies

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import keras
 import streamlit as st
 from PIL import Image
 from patch_generator import smash_n_reconstruct
@@ -23,7 +22,7 @@ def preprocess_input_img(img):
 def generate_ai_prob_score(input_image):
     preprocessed_img = preprocess_input_img(input_image)
 
-    model = keras.models.load_model('classifier.h5')
+    model = tf.keras.models.load_model('classifier.h5')
     ai_score = model.predict(preprocessed_img)
 
     return ai_score
